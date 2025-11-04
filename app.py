6a72b3ff0e241f76be279eb37faf0c2a55970efe
Rudra Raut <rautrudra14@gmail.com>
2025-11-04 12:51:39 +0530

    Fix langchain import error

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-import langchain 
+import langchain
+from langchain.chains import LLMChain
 from langchain.chains.history_aware_retriever import create_history_aware_retriever
 from langchain.chains.retrieval import create_retrieval_chain
 from langchain.chains.combine_documents.stuff import create_stuff_documents_chain
